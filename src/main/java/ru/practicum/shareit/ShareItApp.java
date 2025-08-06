31546965b85ae0b0eb95458a98065dd44dd0ad5a
kl-vl <191594458+kl-vl@users.noreply.github.com>
2025-08-06 17:30:27 +0300

    checkstyle. Unused comment lines clean-up

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("ru.practicum.shareit.item")
 public class ShareItApp {
 
 	public static void main(String[] args) {
